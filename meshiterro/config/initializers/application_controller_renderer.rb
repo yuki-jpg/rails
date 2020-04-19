7db494183190338c4f3390712e10214063078640
yuki-jpg <yabasimxx@gmail.com>
2020-04-19 21:11:44 +0900

    [start/finish]5-4

---
+++ meshiterro/config/initializers/application_controller_renderer.rb
@@ -0,0 +1,10 @@
+# Be sure to restart your server when you modify this file.
+
+# ActiveSupport::Reloader.to_prepare do
+#   ApplicationController.renderer.defaults.merge!(
+#     http_host: 'example.org',
+#     https: false
+#   )
+# end
+
+Refile.secret_key = 'f2d1564d0290ee84989f6f6c04e39868b88d56641032be446e08268890369bfbce16e983f4afebfdb6393bf6082b6fb690e8fbc4247c272888c10253fd559428'
